f3bb1e0a080c606091fcf927475441f59a9cdf29
IamZY <562018301@qq.com>
2020-12-18 16:46:02 +0800

    shell

---
+++ code/5.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# run in back
+# nohup sh 5.sh &
+# nmap -p 80 127.0.0.1 | grep '80/tcp' | awk '{print $2}'
+
+n=`netstat -lntp|grep ':80 '|wc -l`
+if [ $n -eq 0 ]
+then
+	# send email to me	
+fi
+
